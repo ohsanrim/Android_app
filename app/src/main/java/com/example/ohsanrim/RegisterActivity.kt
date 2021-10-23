84c2920d1098786287cbf48a7a515eaba29e6055
ohsanrim <ka28@naver.com>
2021-10-23 19:22:32 +0900

    [2021/10/23] 회원가입페이지 수정
    1. registerActivity.kt 파일 수정
    : 회원가입 버튼 클릭 시 binding 으로view 안의 값을 읽어와 정규식 체크
    진행.
    
    2. viewBinding 그래들 셋팅파일에 적용. (테스트 완료)

--- app/src/main/java/com/example/ohsanrim/RegisterActivity.kt
+++ app/src/main/java/com/example/ohsanrim/RegisterActivity.kt
@@ -3,32 +3,54 @@ package com.example.ohsanrim
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.EditText
+import com.example.ohsanrim.databinding.ActivityRegisterBinding
 
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        //setContentView(R.layout.activity_register)
+        val binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-      //  val rePwText = findViewById <EditText> (R.id.rePwText)
-      //  println("idText: "+idText+" / pwText: "+pwText+" / rePwText: "+rePwText)
+        binding.registerBtn.setOnClickListener{
+            Log.i("register","회원가입 버튼 클릭 이벤트 실행!" )
+            val idText = binding.idText.text
+            val pwText = binding.pwText.text
+            val rePwText = binding.rePwText.text
+            val emailText = binding.emailText.text
+            val phoneText = binding.phoneText.text
 
-
+            if(blankCheck(idText.toString())){
+                //아이디 중복성 체크
+                Log.e("register Fail","ID 값이 공백입니다.  ")
+                Log.e("register Fail","id Check: "+ idText)
+            } else if(blankCheck(pwText.toString()) || blankCheck(rePwText.toString())) {
+                Log.e("register Fail","비밀번호가 공백입니다. 다시한번 확인해주십시오.")
+            } else if(!pwText.toString().equals(rePwText.toString())){
+                //비밀번호 유효성 체크
+                Log.e("register Fail","비밀번호가 일치하지 않습니다. 다시한번 확인해주십시오. ")
+                Log.e("register Fail","pwText: "+pwText+" / rePwText: "+ rePwText)
+            } else if(blankCheck(emailText.toString())){
+                Log.e("register Fail","이메일 란이 공백입니다. 다시한번 확인해주십시오.")
+            } else if(!emailText.contains('@')){
+                //이메일 유효성 체크
+                Log.e("register Fail","이메일 형식이 일치하지 않습니다. 다시한번 확인해주십시오. ")
+                Log.e("register Fail","emailText: "+ emailText)
+            } else if(blankCheck(phoneText.toString())){
+                Log.e("register Fail","핸드폰 번호가 공백입니다. 다시한번 확인해주십시오.")
+            } else if (true){
+                //핸드폰 번호 유효성 체크
+                Log.e("registered...","id Check: "+ idText)
+            } else {
+                Log.e("register success","회원가입 성공!")
+            }
+        }
     }
 
-    fun register_click(view: View){
-//
-//        val idText = findViewById <EditText> (R.id.idText)
-//        val pwText = findViewById <EditText> (R.id.pwText)
-//
-
-
-        //val idText = f
-        val pwText = EditText(findViewById(R.id.pwText))
-        val rePwText = findViewById <EditText> (R.id.rePwText)
-        Log.i("register","idText: "+idText+" / pwText: "+pwText+" / rePwText: "+rePwText )
+    fun blankCheck(value: String): Boolean {
+        return value == "" || value == null
     }
+
 }
